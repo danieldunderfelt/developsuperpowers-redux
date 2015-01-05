fc1b0f1be1a095ed01b06109d0c03ea6e64b4392
Dunderfelt, Daniel <daniel.dunderfelt@activearkjwt.com>
2015-01-05 10:20:03 +0200

    Lorem

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,6 +1,15 @@
 @extends('page')
 @section('page')
 	<h1>
-		Home Page
+		Hello!
 	</h1>
+	<p>
+		I'm Daniel, a web developer from Finland.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium nam ipsa accusantium temporibus sit modi, illum culpa quasi mollitia autem asperiores nesciunt iste ipsum sunt! Quisquam, laborum culpa quod assumenda!
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae ex a odit earum veniam facere quo, deserunt, est, illum voluptatem, fugiat architecto debitis veritatis dignissimos. Obcaecati quidem ratione iste inventore.
+	</p>
 @stop
