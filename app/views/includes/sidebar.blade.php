4617f6184713329ad235166c2bdc34bba4daddd2
Dunderfelt, Daniel <daniel.dunderfelt@activearkjwt.com>
2015-01-05 11:42:16 +0200

    Text edits and style fixes

--- app/views/includes/sidebar.blade.php
+++ app/views/includes/sidebar.blade.php
@@ -6,18 +6,16 @@
 		<h2>
 			Develop<br>Superpowers
 		</h2>
+		<p>
+			The personal site of <strong>Daniel Dunderfelt</strong>.
+		</p>
 		<div class="divider"></div>
 	</header>
 	<article class="about">
 		<p>
-			The personal site of <strong>Daniel Dunderfelt</strong>.
+			I am a passionate web developer with a perpetual interest of learning new things and drive to find and conquer new challenges.
 		</p>
 		<ul>
-			<li>
-				<p>
-					I am a passionate web developer with a perpetual interest of learning new things and conquer new challenges.
-				</p>
-			</li>
 			<li>
 				<p>
 					Employed at:<br>
@@ -34,11 +32,18 @@
 					</a>
 				</p>
 			</li>
-			<li>
+			<li class="cols">
 				<p>
 					Twitter:<br>
 					<a href="https://twitter.com/ddunderfelt">
 						<strong>@ddunderfelt</strong>
+					</a>
+				</p>
+			</li>
+			<li class="cols">
+				<p>
+					git checkout <a href="https://github.com/danieldunderfelt">
+						<strong>danieldunderfelt</strong>
 					</a>
 				</p>
 			</li>
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -5,7 +5,10 @@
 			Hello!
 		</h1>
 		<p>
-			I'm Daniel, a full-stack web developer from Finland.
+			I'm Daniel, a full-stack web developer from Finland. I work with Javascript, PHP and Sass, and my application framework of choice is <a href="http://laravel.com">Laravel</a>.
+		</p>
+		<p>
+			I have some code at <a href="https://github.com/danieldunderfelt">Github</a>, and I create more all the time. I'll populate this site with a full portfolio at some point.
 		</p>
 		<div class="divider small"></div>
 	</article>
